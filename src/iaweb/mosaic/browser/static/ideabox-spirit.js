1f16cf3ae1d00d9310be4913659b9dd068814057
Benoit Suttor <bsuttor@imio.be>
2019-07-24 12:26:05 +0200

    Add static

---
+++ src/iaweb/mosaic/browser/static/ideabox-spirit.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+  var $window = $(window),
+    flexslider = { vars: {} };
+
+  function getGridSize(limit) {
+    var number = limit;
+    if (window.innerWidth < 900) {
+      number = 3;
+    }
+    if (window.innerWidth < 600) {
+      number = 2;
+    }
+    if (window.innerWidth < 480) {
+      number = 1;
+    }
+    return number;
+  }
+
+  $(".flexslider").each(function() {
+    var limit = $(this).attr("data-slider");
+    $(this).flexslider({
+      animation: "slide",
+      animationLoop: false,
+      itemWidth: 210,
+      itemMargin: 5,
+      minItems: getGridSize(limit),
+      maxItems: getGridSize(limit),
+      start: function(slider) {
+        $("body").removeClass("loading");
+      }
+    });
+  });
+});
+
